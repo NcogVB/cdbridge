3422744e205d016360ad01b1e48f02fae3e566b5
NcogVB <vishal.baraiya@ncog.earth>
2025-08-11 14:46:41 +0530

    Update Footer.tsx

--- src/newComponents/Footer.tsx
+++ src/newComponents/Footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
       <div className="container mx-auto md:px-6 px-4 pb-[30px] pt-[56px]">
         <h1 className="w-full text-center text-[20vw] leading-none font-bold text-[#2A8576]">DEX</h1>
         <div className="flex justify-between md:flex-row flex-col-reverse md:items-end mt-[90px] md:gap-4 gap-8">
-          
-          <span> Charts are powered by
+          <div>
+          <span> Charts are powered by 
           <a 
   href="https://in.tradingview.com/" 
   target="_blank" 
@@ -25,7 +25,7 @@ const Footer = () => {
   className="!underline decoration-black text-black font-light text-xs leading-[13.2px] text-[#767676] md:text-start text-center"
 >
   ©2025 Powered by Ncog.Earth
-</a>
+</a> </div>
 
           <div className="flex lg:gap-[90px] md:gap-[50px] gap-[25px] font-normal sm:text-[12px] text-[10px] leading-8 text-[#767676]">
             <div className="flex flex-col md:text-center ">
